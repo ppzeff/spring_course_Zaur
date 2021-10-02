0967989863132636badb3790d0c05f070fde0dd4
ppzeff <ppzeff@gmail.com>
2021-10-02 19:49:28 +0300

    2

--- main/src/com/ppzeff/Dog.java
+++ main/src/com/ppzeff/Dog.java
@@ -1,6 +1,11 @@
 package com.ppzeff;
 
-public class Dog implements Pet{
+public class Dog implements Pet {
+
+    public Dog() {
+        System.out.println("Dog creat");
+    }
+
     @Override
     public void say() {
         System.out.println("Baow-woaw!");
